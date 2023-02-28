1f292d3a5c8454c19a97fca34dedc3a9eabcfa3c
sakushi <maranatha.bruno1997@gmail.com>
2023-02-28 18:01:38 -0300

    class3: swagger

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = "First FastAPI App " #Here we select the title of our App
+app.version = "0.0.2" #Here we can indicate our actual app version 
 
-@app.get('/')
+@app.get('/', tags=['home'])#With tags we can separate our diferents routes by name to identify them easily.
 def message():
     return "Hello world!"
