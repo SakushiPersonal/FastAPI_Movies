84cb11f7cb0dd369274dac2b73fe07320651299d
sakushi <maranatha.bruno1997@gmail.com>
2023-03-02 19:46:00 -0300

    class13: JsonResposne and examples for show in docs

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, Body, Path, Query
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Optional, List
 
 app = FastAPI()
 app.title = "First FastAPI App " #Here we select the title of our App
 app.version = "0.0.5" #Here we can indicate our actual app version 
+
 
 class Movie(BaseModel):
     id: Optional[int] = None
@@ -46,35 +47,48 @@ movies = [
     }
 ]
 
+'''Not really nedded JsonResponse because FastAPI return by default in that format, is just nedded in a specific moments '''
+
+
+#home
 @app.get('/', tags=['home'])#With tags we can separate our diferents routes by name to identify them easily.
 def message():
     return HTMLResponse('<h1>Hello world!</h1>')
 
-@app.get('/movies', tags=['movies'])
-def get_movies():
-    return movies
 
-@app.get('/movies/{id}', tags=['movies'])
-def get_movie(id: int=Path(ge=1, le=200)):
+#get all movies
+@app.get('/movies', tags=['movies'], response_model=List[Movie])
+def get_movies() -> List[Movie]:
+    return JSONResponse(content=movies)
+
+
+#get a movie by id
+@app.get('/movies/{id}', tags=['movies'], response_model=Movie)
+def get_movie(id: int=Path(ge=1, le=200)) -> Movie:
     for movie in movies:
         if movie["id"] == id:
-            return movie
-    return []
+            return JSONResponse(content=movie)
+    return JSONResponse(content={"message": "Couldn't find your movie, try another id"})
 
-@app.get('/movies/', tags=['movies'])#to diferenciate from movies, just add an "/" at the end of the name
-def get_movie_by_category(category: str = Query(min_length=4, max_length=15)):#if I define on the function a parameter but not in the decorator, then automatically is defined as a query request
+
+#get a movie by category
+@app.get('/movies/', tags=['movies'], response_model=List[Movie])#to diferenciate from movies, just add an "/" at the end of the name
+def get_movie_by_category(category: str = Query(min_length=4, max_length=15)) -> List[Movie]:#if I define on the function a parameter but not in the decorator, then automatically is defined as a query request
     movie = list(filter(lambda movie: movie["category"] == category, movies))
-    return movie
+    return JSONResponse(content=movie)
 
-@app.post('/movies', tags=['movies'])
-def create_movie(movie: Movie):
+
+#Create a new movie
+@app.post('/movies', tags=['movies'], response_model=dict)
+def create_movie(movie: Movie) -> dict:
     movies.append(movie)
 
-    return movies
+    return JSONResponse(content={"message": "Movie created successfully!"})
 
 
-@app.put('/movies/{id}',  tags=['movies'])
-def update_movie(id:int, movie: Movie):
+#Modify an existing movie
+@app.put('/movies/{id}',  tags=['movies'], response_model=dict)
+def update_movie(id:int, movie: Movie) ->dict:
     for mov in movies:
         if mov["id"] == id:
             mov["title"] = movie.title
@@ -83,12 +97,15 @@ def update_movie(id:int, movie: Movie):
             mov["rating"] = movie.rating
             mov["category"] = movie.category
 
-            return movies
+            return JSONResponse(content={"message": "Changes saved successfully"})
     
+    return JSONResponse(content={"message": "Movie doesn't exist, please verify your id"})
 
-@app.delete('/movies/{id}', tags=['movies'])
-def delete_movie(id:int):
+
+#Delete a movie by id
+@app.delete('/movies/{id}', tags=['movies'], response_model=dict)
+def delete_movie(id:int) -> dict:
     for movie in movies:
         if movie["id"] == id:
             movies.remove(movie)
-            return movies
+            return JSONResponse(content={"message": "Movie deleted successfully!"})
