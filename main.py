f8c18504e816c726303419563f22153ab7fdfcc1
SakushiPersonal <maranatha.bruno1997@gmail.com>
2023-03-04 17:22:24 -0300

    class2.13: preparing for deploy, railway

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from config.database import engine, Base
 from middleware.error_handler import ErrorHandler
 from routers.movie import movie_router
 from routers.auth import auth_router
+import uvicorn
+import os
 
 
 app = FastAPI()
@@ -16,9 +18,6 @@ app.include_router(auth_router)
 
 Base.metadata.create_all(bind=engine)  
 
-
-
-
 '''Not really nedded JsonResponse because FastAPI return by default in that format, is just nedded in a specific moments '''
 
 
@@ -26,3 +25,8 @@ Base.metadata.create_all(bind=engine)
 @app.get('/', tags=['home'], status_code=status.HTTP_200_OK)#With tags we can separate our diferents routes by name to identify them easily.
 def message():
     return HTMLResponse(status_code=status.HTTP_200_OK, content='<h1>Hello world!</h1>')
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT", 8000)))
